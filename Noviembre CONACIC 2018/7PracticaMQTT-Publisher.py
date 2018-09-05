da68d56522e51a06d3e6437a3bde6b998044eca7
Yael Castro <cyaelcc94@gmail.com>
2018-09-05 16:55:25 +0000

    Deleted Noviembre CONACIC 2018/0EjemploBuzzer.py, Noviembre CONACIC 2018/0EjemploEncoder.py, Noviembre CONACIC 2018/0EjemploLCD.py, Noviembre CONACIC 2018/0EjemploPIR.py, Noviembre CONACIC 2018/1PracticaMRAA.py, Noviembre CONACIC 2018/2PracticaMRAADigital.py, Noviembre CONACIC 2018/3PracticaPIRLCD.py, Noviembre CONACIC 2018/4PracticaEncLCD.py, Noviembre CONACIC 2018/5PracticaPirLCDBuzzer.py, Noviembre CONACIC 2018/6PracticaUbidots1-In.py, Noviembre CONACIC 2018/6PracticaUbidots1.py, Noviembre CONACIC 2018/7PracticaMQTT-Publisher.py, Noviembre CONACIC 2018/7PracticaMQTT-Subscribe.py, Noviembre CONACIC 2018/8PracticaMQTT-PublisherSensores.py, Noviembre CONACIC 2018/8PracticaMQTT-SubscribeSensores.py files

--- Noviembre CONACIC 2018/7PracticaMQTT-Publisher.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#Hace un solo envio
-import paho.mqtt.client as mqtt
-
-mqttc = mqtt.Client()
-mqttc.connect("test.mosquitto.org", 1883)
-
-mqttc.publish("CURSOEdison", "Hola mundo")
-mqttc.loop(3) #timeout = 2s
